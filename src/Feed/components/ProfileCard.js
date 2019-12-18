7eec68a06ce77ab78e0d7d713df0f34d0c7555af
nihaprezz <sergio13prez@gmail.com>
2019-12-18 17:54:32 -0500

    showing recommended friends;

--- src/Feed/components/ProfileCard.js
+++ src/Feed/components/ProfileCard.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom"
+
+const reSize = {
+    width: '10vw', 
+    height: '10vh'
+}
 
 class ProfileCard extends React.Component {
 
     render(){
+        let {id, username, avatar} = this.props.userObject 
         return (
             <div>
-                This is the ProfileCard Component
-                
+                <p>id : {id}</p>
+                <p>username : {username}</p>
+                <Link to={`/user/${id}`}><img style={reSize} alt="profile-pic" src={avatar}></img></Link>
             </div>
         )
     }
--- src/Feed/containers/RecommendedContainer.js
+++ src/Feed/containers/RecommendedContainer.js
@@ -1,16 +1,66 @@
 import React from "react";
 import ProfileCards from "../components/ProfileCard"
+import { connect } from "react-redux"
+import { fetchingAllUserInfo } from "../../redux/actions"
 
 class RecommendedContainer extends React.Component {
+    constructor(){
+        super();
+
+        this.state  = {
+            alikeUsers: []
+        }
+    }
+
+    componentDidMount(){
+        fetch("http://localhost:3001/user/recommendations", {
+            headers: {
+                "Authorization" : `Bearer ${localStorage.getItem('jwt')}`
+            }
+        })
+        .then(resp => resp.json())
+        .then( friends => {
+            this.setState({alikeUsers: friends})
+        })
+
+        this.props.fetchingAllUserInfo()
+    }
 
     render(){
+        let friendsIds = this.props.currentFriends.map(friend =>  friend.id)
+
+        let duplicates = []
+
+        let uniqueFriends = []
+        //FILTERS OUT THE ARRAY TO MAKE SURE NO USERS THAT ARE ALREADY FRIENDS SHOW UP
+        for (let user of this.state.alikeUsers) {
+            if (friendsIds.includes(user.id)) {
+              duplicates.push(user)
+            } else {
+              uniqueFriends.push(user)
+            }
+        }
+
         return (
             <div>
-                This is the RecommendedContainer Component
-                < ProfileCards />
+                {uniqueFriends.map( user => {
+                    return < ProfileCards key={user.id} userObject={user} />
+                })}
             </div>
         )
     }
 }
 
-export default RecommendedContainer
+const mapStateToProps = state => {
+    return {
+        currentFriends: state.usersFriends
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchingAllUserInfo: () => {dispatch(fetchingAllUserInfo())}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecommendedContainer)
