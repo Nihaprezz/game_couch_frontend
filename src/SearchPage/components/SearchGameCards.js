d25ddfe656cb0ff61099aaaa79abdd54bfe23802
nihaprezz <sergio13prez@gmail.com>
2019-12-23 17:25:50 -0500

    finished search page styling

--- src/SearchPage/components/SearchGameCards.js
+++ src/SearchPage/components/SearchGameCards.js
@@ -2,11 +2,25 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 class SearchGameCards extends React.Component {
+
+
     render(){
+        let {name, background_image} = this.props.gameObj
+
+        const cardStyle = {
+            background: `url(${background_image})`,
+            backgroundSize: '100% 100%',
+            backgroundPosition: 'center center',
+            width: 'auto',
+            height: '300px'
+        }
+
         return (
-            <div>
-                SearchGameCards :
-                <Link to={`/game/${this.props.gameObj.id}`}>{this.props.gameObj.name}</Link>
+            <div className="search-game-card">
+                <Link to={`/game/${this.props.gameObj.id}`}>
+                <div style={cardStyle} className="search-game-card-image"></div>
+                </Link>
+                <Link className="search-game-text" to={`/game/${this.props.gameObj.id}`}>{name}</Link>
             </div>
         )
     }
