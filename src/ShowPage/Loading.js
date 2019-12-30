83e59aace5c92ba11e3741086ee7f982645f0b20
nihaprezz <sergio13prez@gmail.com>
2019-12-30 16:49:59 -0500

    added loading animation

--- src/ShowPage/Loading.js
+++ src/ShowPage/Loading.js
@@ -1,28 +1,38 @@
 import React from "react";
 import { usePromiseTracker } from "react-promise-tracker";
-import DotLoader from "react-spinners/DotLoader";
-import { css } from "@emotion/core";
+import Lottie from 'react-lottie';
+import animationData from "../styles/gameLoader.json"
+
 
 export const LoadingSpinerComponent = (props) => {
      const { promiseInProgress } = usePromiseTracker();
 
-     const override = css`
-        display: block;
-        margin: 0 auto;
-        border-color: red;
-        `;
+      const options = {
+        loop: true,
+        autoplay: true, 
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+      };
      
       return (
         <div>
         {
           (promiseInProgress === true) ?
-            <div className="sweet-loading">
-            <DotLoader
-            css={override}
-            size={700} 
-            color={"#36D7B7"}
-            />
-            </div>
+            // <div className="sweet-loading">
+            // <DotLoader
+            // css={override}
+            // size={700} 
+            // color={"#36D7B7"}
+            // />
+            // </div>
+            <Lottie options={options}
+              height={400}
+              width={400}
+              isStopped={false}
+              isPaused={false}
+              />
           :
             null
         }
